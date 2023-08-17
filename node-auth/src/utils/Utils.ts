50c43051aaebd3891c8893bd3fd519480cf07015
Matt Jennings <matt@mattjennings.net>
2023-08-16 21:09:41 -0700

    got rid of unneeded code

--- node-auth/src/utils/Utils.ts
+++ node-auth/src/utils/Utils.ts
@@ -3,6 +3,6 @@ import path from 'path';
 export default class Utils {
   // Return absolute directory path
   getDirRootPath() {
-    return path.resolve(`${process.cwd()}`);
+    return path.resolve(process.cwd());
   }
 }
