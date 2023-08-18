694068428b2733f879211ea5f0617b9a6050c130
Matt Jennings <matt@mattjennings.net>
2023-08-17 19:30:20 -0700

    updatded App.tsx

--- react-auth/src/components/App/App.tsx
+++ react-auth/src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './LoginRegisterForm.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Login from '../Login/Login';
@@ -8,13 +8,13 @@ import Nav from '../Nav/Nav';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
       <Nav />
 
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
