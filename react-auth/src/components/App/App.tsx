ca2af29ff936b5ccad716afc100ff7369ef01819
Matt Jennings <matt@mattjennings.net>
2023-08-17 17:25:45 -0700

    added new nav component

--- react-auth/src/components/App/App.tsx
+++ react-auth/src/components/App/App.tsx
@@ -1,28 +1,20 @@
 import React from 'react';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './LoginRegisterForm.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Route>
-  )
-);
+import Nav from '../Nav/Nav';
 
 export default function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    <BrowserRouter>
+      <Nav />
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
