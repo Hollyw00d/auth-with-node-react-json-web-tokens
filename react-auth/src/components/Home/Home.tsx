5cff4c9ade5480c787f15f1e0ab9306c7cfe818d
Matt Jennings <matt@mattjennings.net>
2023-08-18 16:20:50 -0700

    made it possible to log in

--- react-auth/src/components/Home/Home.tsx
+++ react-auth/src/components/Home/Home.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  const [message, setMessage] = useState('You are not logged in.');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/user', {
+          withCredentials: true
+        });
+
+        const user = await response.data;
+
+        setMessage(`Hi ${user.first_name} ${user.last_name}`);
+      } catch (error) {
+        setMessage('Your are not logged in.');
+      }
+    })();
+  }, []);
+
   return (
     <div className="d-flex align-items-center py-4 bg-body-tertiary">
       <main className="form-signin w-100 m-auto">
-        <p>Hello</p>
+        <h3>{message}</h3>
       </main>
     </div>
   );
